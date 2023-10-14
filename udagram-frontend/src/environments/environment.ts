1152694a258bca18a8e4244e4be8d39c7f429756
TuanVi <phantuanvi.com>
2023-10-14 20:36:04 +0800

    Container Orchestration with Kubernetes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ad2b4c0c5d20d42fbb6a2cdeb2a6a294-304839403.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
